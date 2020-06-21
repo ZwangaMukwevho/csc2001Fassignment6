93616f87c597ccad789f1cf878ac61069822c186
ZwangaMukwevho <zwangatm3@gmail.com>
2020-06-21 14:00:31 +0200

    first commit

---
+++ Test.java
@@ -0,0 +1,76 @@
+
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Queue;
+import java.util.Map;
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Test{ 
+public static void main( String [ ] args )
+    {
+        
+        try
+        {   	
+            //FileReader fin = new FileReader(args[0]);
+        	FileReader fin = new FileReader("Graph2.txt");
+            Scanner graphFile = new Scanner( fin );
+	
+	//Skip first line
+	graphFile.nextLine();
+
+	    // Counter used in filtering lines from input to seperate different lines based on what they represent
+		int count = 0;
+		int num = 0;
+		String temp = "";
+
+            // Read the edges and insert
+            String line;
+            while( graphFile.hasNextLine( ) )
+            {
+                line = graphFile.nextLine( );
+                StringTokenizer st = new StringTokenizer( line );
+		num = st.countTokens( )/3;
+
+                    if( num >= 3  )
+                    {
+			temp = st.nextToken( );
+			for (int i = 0; i< num;i++){
+				System.out.println(temp);
+				System.out.println(st.nextToken( ));
+				System.out.println(st.nextToken( ));
+				System.out.println("");
+				}
+                        continue;
+                    }
+
+		else{
+			System.out.println( "Number of Hospital " + line );
+			line = graphFile.nextLine( );
+			System.out.println( "Hospitals " + line );
+			line = graphFile.nextLine( );
+			System.out.println( "Number of victims " + line );
+			line = graphFile.nextLine( );
+			System.out.println( "victims " + line );
+			
+			}
+
+                    //String source  = st.nextToken( );
+                    //String dest    = st.nextToken( );
+                    //int    cost    = Integer.parseInt( st.nextToken( ) );
+                    //g.addEdge( source, dest, cost );
+                
+             }
+         }
+         catch( IOException e )
+           { System.err.println( e ); }
+
+        
+             
+    }}
